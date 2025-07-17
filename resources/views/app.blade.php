23e00ef4111956f28e4551319512ca67c15e1553
Vins Mark Adlawan <vnsmrkdlwn@gmai.com>
2025-07-17 11:52:28 +0800

    change the favicon in the tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,9 +33,9 @@
 
     <title inertia>{{ config('app.name', 'SDNHS-LMS') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/lmslogoo.png" sizes="any">
+    <link rel="icon" href="/lmslogoo.png">
+    <link rel="apple-touch-icon" href="/lmslogoo.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
